b37d42fa42788c407185a694d02ae4cf50c786fb
DavidCodeRussia <93557790+DavidCodeRussia@users.noreply.github.com>
2023-01-12 23:34:34 +0400

    added filtration by search url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,50 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
+import React from 'react';
+import ReactPaginate from 'react-paginate';
 
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/Skeleton";
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/Skeleton';
 
 const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [filteredItems, setFilteredItems] = React.useState([]);
   const skeletons = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
+  const pizzas = items
+    // .filter((item) => {
+    //   if (item.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())) {
+    //     return true;
+    //   }
+    //   return false;
+    // })
+    .map((item, index) => <PizzaBlock key={index} {...item} />);
 
   const [sort, setSort] = React.useState({
-    name: "popular (desc)",
-    sortProperty: "rating",
-    direction: "desc",
+    name: 'popular (desc)',
+    sortProperty: 'rating',
+    direction: 'desc',
   });
-  const [category, setCategory] = React.useState("");
+  const [category, setCategory] = React.useState('');
   const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     setLoading(true);
 
     const categoryId = category > 0 ? `category=${category}` : ``;
-    const sortApi = sort.sortProperty.replace("-", "");
-    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
+    const sortApi = sort.sortProperty.replace('-', '');
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     fetch(
-      `https://63bbd74a32d17a509099ef50.mockapi.io/items?${categoryId}&sortBy=${sortApi}&order=${order}`,
+      `https://63bbd74a32d17a509099ef50.mockapi.io/items?${categoryId}&sortBy=${sortApi}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((data) => {
         setItems(data);
         setLoading(false);
       });
-  }, [category, sort]);
+  }, [category, sort, searchValue]);
 
   return (
     <div className="container">
@@ -43,18 +53,16 @@ const Home = ({ searchValue }) => {
         <Sort sort={sort} setSort={setSort} />
       </div>
       <h2 className="content__title">All pizzas</h2>
-      <div className="content__items">
-        {loading
-          ? skeletons
-          : items
-              .filter((item) => {
-                if (item.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())) {
-                  return true;
-                }
-                return false;
-              })
-              .map((item, index) => <PizzaBlock key={index} {...item} />)}
-      </div>
+      <div className="content__items">{loading ? skeletons : pizzas}</div>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={(e) => console.log('e', e)}
+        pageRangeDisplayed={5}
+        pageCount={3}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 };
